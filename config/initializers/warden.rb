a6e804024403c6cf23a554dfbdbe57ef8dc67784
naidu479 <naidu479@gmail.com>
2016-10-20 00:54:25 +0530

    added loyalty

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -4,6 +4,9 @@ Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
     if user.is_a?(Spree::User)
       Spree::Order.incomplete.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
         order.associate_user!(user)
+        loyalty_points_earned = order.loyalty_points_for(order.total)
+        order.create_credit_transaction(loyalty_points_earned)
+        order.redeem_loyalty_points
       end
     end
   end
